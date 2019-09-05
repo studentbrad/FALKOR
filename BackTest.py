c28fc7e126868f98e9444c119df4a5eca29165c9
Vladimir Dyagilev <vdyagilev5@gmail.com>
2019-09-04 22:06:18 -0400

    added backtest functionality

--- BackTest.py
+++ BackTest.py
@@ -1,11 +1,15 @@
-from Gekko.Gekko import Gekko
-from api_wrappers.APIWrapper import APIWrappers
+from Gekko import Gekko
+from api_wrappers.APIWrapper import APIWrapper
 from api_wrappers.BinanceWrapper import BinanceWrapper
 from strategies import CNN_Strategy
-
-from helpers import data_processing
+from Portfolio import Portfolio
+from .helpers.data_processing import *
 
 from pandas import DataFrame
+
+from strategies.example_strategies import ModelStrat
+from .helpers.saving_models import save_model, load_model
+from models.GRU.GRU import GRUnet
 
 class BackTest:
 	"""
@@ -27,56 +31,22 @@ class BackTest:
 		self.portfolio = Portfolio()
 		self.gekko = Gekko(self.portfolio)
 
-	def get_historical_candles(self, api_wrapper: APIWrapper, symbol: str, interval:str, start_time: str, end_time: str) -> DataFrame:
-		"""Returns a DataFrame of containing ochlv candles"""
-		
-		return api_wrapper.historical_candles(symbol, interval, start_time, end_time)
-
-	def preprocess_data(self, candles_df: DataFrame) -> DataFrame
+	def profit_test(self, model, candles_df):
 		"""
-		Returns a DataFrame oc ochlv candles with specified processing
-		steps performed ex. added technical indicators
+		Runs a profitability test of model on historical data of candles_df. 
+		By simulating a trade at every model signal, we sum profits/losses made from
+		all trade signals generated. Returns string of profit stats
 		"""
-		return data_processing.add_ti()
 
-	def split_data(self, df: DataFrame, period: int, step_size: int) -> List(DataFrame), List(float):
-		"""
-		Returns a tuple of Lists. The first is a List of DataFrames, each 
-		being an input. The second is a List of ints, each being the 
-		price for the Security at period = curr_period + 5
-		"""
-		df_splits = data_processing.split_dataset(df)
-		price_splits = data_processing.price_labels(df_splits)
-
-		# ensure len(df_splits) == len(price_splits)
-		df_splits = df_splits[:len(price_splits)]
-
-		return df_splits, price_splits
-
-	def run(self):
-		# Initialize what we are backtesting 
-		api_wrapper = BinanceWrapper("id", "secret")
-
-		symbol, interval = 'ETHBTC', '1m'
-		start_time, end_time = 'January 1 2018', 'January 1 2019'
+		model_strat = ModelStrat(model)
+		return "{}".format(self.gekko.backtest(model_strat, candles_df))
 
 
-		# Get historical OCHLV candlestick DataFrame
-		candles_df = self.get_historical_candles(api_wrapper, symbol, interval,
-											start_time, end_time)
+bw = BinanceWrapper('5lJ0uGit9PuUxHka3hBWhPmsi7dWyxEwvEntUZFKmm0xfNz3VjHWi5WSr5W1VBJV','BFWVs8ko7Cd4sjdQ9amGJTnToGWy9TbQWIjeorSCj23FGiwFaknzkgLPcrgWrxsw')
 
-		# Perform Preprocessing steps on the data
-		processed_df = self.preprocess_data(candles_df)
+gru = GRUnet(num_features=12, num_rows=30, batch_size=64, hidden_size=500, num_layers=5).float()
+load_model(gru, 'gru_weights')
 
-		# Get a list of input df's and their corresponding future price
-		df_splits, price_splits = self.split_data(df=processed_df, period=30, step_size=5)
+strat = ModelStrat(gru)
 
-		# Run a backtest with profitabililty tracking on df_splits and price_splits
-		results = self.gekko.backtest(df_splits, price_splits)
-
-		# Show backtesting results. Profitabilty, trades made,
-		print(results)
-
-if __name__ == '__main__':
-    bt = BackTest()
-    bt.run()
+b = BackTest(bw, strat)
--- strategies/Strategy.py
+++ strategies/Strategy.py
@@ -2,17 +2,8 @@
 class Strategy:
     """Abstract class representing a Strategy used by Gekko. The child class must create all NotImplemented methods"""
 
-    def feed_data(live_df):
-        """Feed in a DataFrame of the last 30 ochl periods."""
-
-        raise NotImplementedError
-
-    def generate_signals():
+    def generate_signals(self):
         """Returns a list of trading signals"""
 
         raise NotImplementedError
 
-    def update():
-        """Run whatever operations necessary to keep the strategy up-to-date with current data"""
-
-        raise NotImplementedError
